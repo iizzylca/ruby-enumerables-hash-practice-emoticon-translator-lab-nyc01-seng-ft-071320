bfaab4dba5cb4c89be3b75de0b9c282a4f482398
Israel Canessa <israellcanessa@gmail.com>
2020-07-10 04:21:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,7 +34,7 @@ end
 def get_english_meaning(path, emoticon)
   library = load_library(path)
   update = nil
-  library.each do |value, name|
+  library.each do |value, names|
     binding.pry
   end
 
