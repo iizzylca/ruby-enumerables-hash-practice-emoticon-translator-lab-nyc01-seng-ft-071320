8c15a3327cc0c3d961f6d5784eddb6def69c1e11
Israel Canessa <israellcanessa@gmail.com>
2020-07-10 02:25:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require 'pry'
 def load_library(path)
   library = YAML.load_file(path)
   #binding.pry
-  update = {'get_meaning'  => {},
+  update = {'get_meaning'=> {},
                     'get_emoticon' => {} }
 
   library.each do |meaning, values|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
