4591f4c396e3b377c07d5cc3c3a434efd69c7732
Israel Canessa <israellcanessa@gmail.com>
2020-07-10 04:13:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(path)
     update[names] = {}
     update[names][:english] = values[0]
     update[names][:japanese] = values[1]
-    binding.pry
+    #binding.pry
   end
   update
   #binding.pry
