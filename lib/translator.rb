d5528a48d7645ea1f142205eea6b9df9cd9e27de
Israel Canessa <israellcanessa@gmail.com>
2020-07-10 02:39:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(path)
   library.each do |names, values|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
     update['get_name'][values[1]] = names
     update['get_emoticon'][values[0]] = values[1]
-    binding.pry
+    #binding.pry
   end
   update
 end
