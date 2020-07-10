3a3bd88a49865d8b0cbbfee4c34dcd48de1a2864
Israel Canessa <israellcanessa@gmail.com>
2020-07-10 01:37:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,23 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
-end
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  new_emoticon = {'strip_meaning' => {}, 'strip_value' => {} }
+
+  emoticons.each do |meaning , value|
+    english_value = value[0]
+    japanese_value = value[1]
+    binding.pry
+    new_emoticon['strip_meaning'][japanese_value] = meaning
+    mew_emoticon['string_value'][english] = japanese
+
+  end
+
+
+  end
+
+
 
 def get_japanese_emoticon
   # code goes here
