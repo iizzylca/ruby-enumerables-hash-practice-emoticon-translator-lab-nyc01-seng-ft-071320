c3a1b32dc39fe00e8ddcd3417046d88eb92913c8
Israel Canessa <israellcanessa@gmail.com>
2020-07-10 02:38:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,7 +30,7 @@ end
 def get_english_meaning(path, japanese_emoticon)
   library = load_library(path)
   update = nil
-  library['get_name'].each do |value, name |
+  library['get_name'].each do |value, name|
     #binding.pry
     if value == japanese_emoticon
       update = meaning
