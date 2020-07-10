9a5f4bac608904168e3e78874ebb972e86ce9368
Israel Canessa <israellcanessa@gmail.com>
2020-07-10 02:00:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,6 +34,8 @@ def get_english_meaning(path, japanese_emoticon)
   end
   if output == nil
     return "Sorry, that emoticon was not found"
-  else    return output
+  else
+    #binding.pry
+    return output
   end
 end
